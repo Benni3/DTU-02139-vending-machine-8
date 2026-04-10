b4d4e5f5605bbfabeb5d9940379a283a700cb2d3
wrl6765 <william.riis.laursen@gmail.com>
2026-04-10 16:15:44 +0200

    Refactor VendingMachine to use Boolean types for coin inputs and remove unused Price and Sum modules

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,9 @@
 class VendingMachine extends Module {
     val io = IO(new Bundle {
-        val coin2 = Input(UInt(8.W))
-        val coin5 = Input(UInt(8.W))
-        val buy = Input(bool())
+        val coin2 = Input(Bool())
+        val coin5 = Input(Bool())
+        val buy = Input(Bool())
         val sel = Input(UInt(3.W))
-        val coin5input = Input(Bool())
-        val coin2input = Input(Bool())
         val alarm = Output(UInt(8.W))
         val releaseCan = Output(UInt(8.W))
         
@@ -15,25 +13,28 @@ class VendingMachine extends Module {
     val sum = RegInit(0.U(8.W))
     sum := sum + io.coin2 + io.coin5
 
-    ////// price afhænger af basys 3 input (kodes seneere )
+    ////// price afhænger af basys 3 input (kodes senere )
     
     val price = RegInit(0.U(8.W))
-    when (coin5input) {
+    when (io.coin5) {
         price := price + 5.U
-    } .elsewhen (coin2input) {
+    } .elsewhen (io.coin2) {
         price := price + 2.U
     }
 
     io.alarm := 0.U
     io.releaseCan := 0.U
 
-    when(io.buy === 1.U) {
+    when(io.buy) {
         when(sum >= price) {
                 io.releaseCan := 1.U
                 sum := 0.U
         } .otherwise {
                 io.alarm := 1.U
-        }
-
-        
+        }   
     }
+
+    //when alarm is 1, then it should light up the red led on basys 3, and when release can is 1, then it should light up the green led on basys 3.
+
+    
+}
